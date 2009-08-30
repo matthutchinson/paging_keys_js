ac421f9f20970e6e591a8fe4ed67b36ac04ec324
Matthew Hutchinson <matt@hiddenloop.com>
2009-08-30 21:15:19 +0100

    fixed wording on menu and js init call

--- javascripts/paging_keys.js
+++ javascripts/paging_keys.js
@@ -1,7 +1,82 @@
+/*  from http://la.ma.la/blog/diary_200511041713.htm - hotkey.js */
+
+function HotKey(element){
+	this.target = element || document;
+	this._keyfunc = {};
+	this.init();
+}
+HotKey.kc2char = function(kc){
+	var between = function(a,b){
+		return a <= kc && kc <= b
+	}
+	var _32_40 = "space pageup pagedown end home left up right down".split(" ");
+	var kt = {
+		8  : "back",
+		9  : "tab"  ,
+		13 : "enter",
+		16 : "shift",
+		17 : "ctrl",
+		46 : "delete"
+	};
+	return (
+		between(65,90)  ? String.fromCharCode(kc+32) : // a-z
+		between(48,57)  ? String.fromCharCode(kc) :    // 0-9
+		between(96,105) ? String.fromCharCode(kc-48) : // num 0-9
+		between(32,40)  ? _32_40[kc-32] :
+		kt.hasOwnProperty(kc) ? kt[kc] :
+		null
+	)
+}
+HotKey.prototype.ignore = /input|textarea/i;
+HotKey.prototype.init = function(){
+	var self = this;
+	var listener = function(e){
+		self.onkeydown(e)
+	};
+	if(this.target.addEventListener){
+		this.target.addEventListener("keydown", listener, true);
+	}else{
+		this.target.attachEvent("onkeydown", listener)
+	}
+}
+HotKey.prototype.onkeydown = function(e){
+	var tag = (e.target || e.srcElement).tagName;
+	if(this.ignore.test(tag)) return;
+	var input = HotKey.kc2char(e.keyCode);
+
+	if(e.shiftKey && input.length == 1){
+		input = input.toUpperCase()
+	}
+	if(e.altKey) input = "A" + input;
+	if(e.ctrlKey) input = "C" + input;
+
+	if(this._keyfunc.hasOwnProperty(input)){
+		this._keyfunc[input].call(this,e)
+	}
+}
+HotKey.prototype.sendKey = function(key){
+	this._keyfunc[key] && this._keyfunc[key]()
+}
+HotKey.prototype.add = function(key,func){
+	if(key.constructor == Array){
+		for(var i=0;i<key.length;i++)
+			this._keyfunc[key[i]] = func;
+	}else{
+		this._keyfunc[key] = func;
+	}
+}
+HotKey.prototype.remove = function(key){
+	if(key.constructor == Array){
+		for(var i=0;i<key.length;i++)
+			this._keyfunc[key[i]] = function () {};
+	}else{
+		this._keyfunc[key] = function () {};
+	}
+}
+
+
 /* https://github.com/hiddenloop/paging_keys_js - inspired by navigation on ffffound.com UI */
 /* by Matthew Hutchinson - hiddenloop.com */
-
-pagingKeys.init();
 
 var pagingKeys = function() {
 	
@@ -264,84 +339,9 @@ var pagingKeys = function() {
   }
 }();
 
-
 // Are all the variable and function names logical and easy to understand?
 // Is the code logically structured? Can you "read" it from top to bottom?
 // Are the dependencies obvious?
 // Have you commented areas that might be confusing?
 
-/*  from http://la.ma.la/blog/diary_200511041713.htm - hotkey.js */
-
-function HotKey(element){
-	this.target = element || document;
-	this._keyfunc = {};
-	this.init();
-}
-HotKey.kc2char = function(kc){
-	var between = function(a,b){
-		return a <= kc && kc <= b
-	}
-	var _32_40 = "space pageup pagedown end home left up right down".split(" ");
-	var kt = {
-		8  : "back",
-		9  : "tab"  ,
-		13 : "enter",
-		16 : "shift",
-		17 : "ctrl",
-		46 : "delete"
-	};
-	return (
-		between(65,90)  ? String.fromCharCode(kc+32) : // a-z
-		between(48,57)  ? String.fromCharCode(kc) :    // 0-9
-		between(96,105) ? String.fromCharCode(kc-48) : // num 0-9
-		between(32,40)  ? _32_40[kc-32] :
-		kt.hasOwnProperty(kc) ? kt[kc] :
-		null
-	)
-}
-HotKey.prototype.ignore = /input|textarea/i;
-HotKey.prototype.init = function(){
-	var self = this;
-	var listener = function(e){
-		self.onkeydown(e)
-	};
-	if(this.target.addEventListener){
-		this.target.addEventListener("keydown", listener, true);
-	}else{
-		this.target.attachEvent("onkeydown", listener)
-	}
-}
-HotKey.prototype.onkeydown = function(e){
-	var tag = (e.target || e.srcElement).tagName;
-	if(this.ignore.test(tag)) return;
-	var input = HotKey.kc2char(e.keyCode);
-
-	if(e.shiftKey && input.length == 1){
-		input = input.toUpperCase()
-	}
-	if(e.altKey) input = "A" + input;
-	if(e.ctrlKey) input = "C" + input;
-
-	if(this._keyfunc.hasOwnProperty(input)){
-		this._keyfunc[input].call(this,e)
-	}
-}
-HotKey.prototype.sendKey = function(key){
-	this._keyfunc[key] && this._keyfunc[key]()
-}
-HotKey.prototype.add = function(key,func){
-	if(key.constructor == Array){
-		for(var i=0;i<key.length;i++)
-			this._keyfunc[key[i]] = func;
-	}else{
-		this._keyfunc[key] = func;
-	}
-}
-HotKey.prototype.remove = function(key){
-	if(key.constructor == Array){
-		for(var i=0;i<key.length;i++)
-			this._keyfunc[key[i]] = function () {};
-	}else{
-		this._keyfunc[key] = function () {};
-	}
-}
+pagingKeys.init();
